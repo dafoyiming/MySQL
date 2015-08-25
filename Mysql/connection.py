523a711e97a99a7bc58f32420d5a06b271d846d7
dafoyiming <dafoyiming@gmail.com>
2015-08-25 23:19:30 +0800

    修改connect

--- Mysql/connection.py
+++ Mysql/connection.py
@@ -1,11 +1,11 @@
-import MySQLdb
+﻿import MySQLdb
 
 conn = MySQLdb.connect(
                         host = '127.0.0.1',
                         port = 3306,
                         user = 'root',
                         passwd = 'DAfo4946816',
-                        db = 'zym',
+                        db = 'zzdb',
                         charset = 'utf8'
                         )
 
